c7886a24f11dffb5eb6492be102efa5fab20180e
bazev <evan.bazin@gmail.com>
2021-11-23 11:06:37 +0100

    maj

--- src/main/java/fr/humanbooster/fx/poussettes/coordination/PriceRequestBean.java
+++ src/main/java/fr/humanbooster/fx/poussettes/coordination/PriceRequestBean.java
@@ -1,6 +1,8 @@
 package fr.humanbooster.fx.poussettes.coordination;
 
 
+import fr.humanbooster.fx.poussettes.business.DemandeDePrix;
+import fr.humanbooster.fx.poussettes.business.Poussette;
 import fr.humanbooster.fx.poussettes.service.DemandeDePrixService;
 import fr.humanbooster.fx.poussettes.service.PoussetteService;
 import fr.humanbooster.fx.poussettes.service.impl.DemandeDePrixServiceImpl;
@@ -8,15 +10,22 @@ import fr.humanbooster.fx.poussettes.service.impl.PoussetteServiceImpl;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import java.util.List;
 
 @ManagedBean(name = "priceRequest")
 @RequestScoped
 public class PriceRequestBean {
     private DemandeDePrixService demandeDePrixService = new DemandeDePrixServiceImpl();
     private PoussetteService poussetteService = new PoussetteServiceImpl();
+    private List<Poussette> poussetteList = null;
+    private DemandeDePrix demandeDePrix = null;
 
     public PriceRequestBean() {
+        poussetteList = poussetteService.recupererPoussettes();
+    }
 
+    public String SendRequest() {
+        demandeDePrix = demandeDePrixService.ajouterDemandeDePrix(demandeDePrix.getEmail(), demandeDePrix.getPoussetteSelectionnee(), demandeDePrix.getDateDebut(), demandeDePrix.getDateFin(), demandeDePrix.getInformationComplementaire())
     }
 }
 
